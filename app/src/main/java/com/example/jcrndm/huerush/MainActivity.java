f5affa757a56870ab2e6cb98b879e1588b46971d
JCrndm <jcrndm@Jimmys-Air.attlocal.net>
2018-03-25 13:04:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jcrndm/huerush/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jcrndm.huerush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
